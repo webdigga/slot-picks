2043844640c603d48d94593287c71ae67ca04ff9
webdigga <webdigga42@gmail.com>
2024-09-16 11:40:14 +0100

    Updating cookie banner to work without Alpine

--- src/_includes/static/js/script.js
+++ src/_includes/static/js/script.js
@@ -36,3 +36,44 @@ if(menuButton) {
     }
   });
 }
+
+/*
+  Cookie consent
+*/
+if(document.body.classList.contains('prod')) {
+  const cookieBanner = document.querySelector('[data-test="cookieBanner"]');
+
+  if(!localStorage.getItem('consentGranted')) {
+    cookieBanner.classList.remove('hidden');
+  }
+
+  document.querySelector('[data-test="consentButton"]').addEventListener('click', () => {
+    localStorage.setItem('consentGranted', true);
+
+    loadGtagScript();
+
+    cookieBanner.classList.add('hidden');
+  });        
+
+  if(localStorage.getItem("consentGranted")) {
+    loadGtagScript();
+  }
+
+  function loadGtagScript() {
+    function gtag() { dataLayer.push(arguments); }
+
+    gtag('consent', 'update', {
+      ad_user_data: 'granted',
+      ad_personalization: 'granted',
+      ad_storage: 'granted',
+      analytics_storage: 'granted'
+    });
+
+    const gtagScript = document.createElement('script');
+    gtagScript.defer = true;
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=GTAG_ID';
+
+    const firstScript = document.getElementsByTagName('script')[0];
+    firstScript.parentNode.insertBefore(gtagScript,firstScript);
+  }
+}
