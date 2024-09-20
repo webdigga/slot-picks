e1c0c89bb59da17b73b76bb19f13859255074dd3
webdigga <webdigga42@gmail.com>
2024-09-20 18:20:17 +0100

    Adding call me behaviour

--- src/_includes/static/js/callMeButtonBehaviour/callMeButtonBehaviour.js
+++ src/_includes/static/js/callMeButtonBehaviour/callMeButtonBehaviour.js
@@ -2,14 +2,7 @@
   Changing behaviour of call me button depending on if mobile or not
 */
 function callMeButtonBehaviour() {
-  const button = document.querySelector('[data-test="callMeButton"]');
-
-  if (!button) {
-    console.error('Button not found when trying to attach event listener');
-    return;
-  }
-
-  button.addEventListener("click", (event) => {
+  document.querySelector('[data-test="callMeButton"]').addEventListener("click", (event) => {
     event.preventDefault();
     window.location.href = /Mobi|Android/i.test(navigator.userAgent) ? event.target.href : '/contact/#contact';
   });
