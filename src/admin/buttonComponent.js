4e78402249c2ec96a6c113e8cc15af47a37c9d93
webdigga <webdigga42@gmail.com>
2024-10-05 17:45:20 +0100

    Fix for button

--- src/admin/buttonComponent.js
+++ src/admin/buttonComponent.js
@@ -13,7 +13,7 @@ CMS.registerEditorComponent({
     {
       name: 'buttonText',
       label: 'Button Text',
-      widget: 'markdown'
+      widget: 'string'
     },
     {
       label: 'Button Type',
@@ -40,7 +40,9 @@ CMS.registerEditorComponent({
   //
   // Additionally, it's recommended that you use non-greedy capturing groups (e.g.
   // `(.*?)` vs `(.*)`), especially if matching against newline characters.
-  pattern: /^<a href="(.*?)" class="(.*?)">$\s*?(.*?)^<\/a>$/ms,
+
+  // Example with multiline - pattern: /^<a>$\s*?<summary>(.*?)<\/summary>\n\n(.*?)\n^<\/a>$/ms,
+  pattern: /^<a href="(.*?)" class="(.*?)">(.*?)<\/a>$/s,
   // Given a RegExp Match object
   // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match#return_value),
   // return an object with one property for each field defined in `fields`.
@@ -59,19 +61,11 @@ CMS.registerEditorComponent({
   // This is used to serialize the data from the custom widget to the
   // markdown document
   toBlock: function(data) {
-    return `
-<a href="${ data.destination }" class="${ data.buttonType }">
-  ${data.buttonText}
-</a>
-`;
+    return `<a href="${ data.destination }" class="${ data.buttonType }">${data.buttonText}</a>`;
   },
   // Preview output for this component. Can either be a string or a React component
   // (component gives better render performance)
   toPreview: function(data) {
-    return `
-<a href="${ data.destination }" class="${ data.buttonType }">
-  ${data.buttonText}
-</a>
-`;
+    return `<a href="${ data.destination }" class="${ data.buttonType }">${data.buttonText}</a>`;
   }
 });
--- src/articles/spinal-screening.md
+++ src/articles/spinal-screening.md
@@ -22,9 +22,4 @@ Spinal Screenings are for everyone, but specifically for anyone who experiences 
 * Tension and Inability to relax
 * Poor Posture
 
-
-<a href="/sign-up-for-a-spinal-screening/" class="no-underline self-end mb-3 rounded-md cursor-pointer inline-block bg-[color:var(--light-green)] text-white py-4 px-6 hover:opacity-80 border border-solid border-white">
-  
-  Sign up now!
-
-</a>
+<a href="/sign-up-for-a-spinal-screening/" class="no-underline self-end mb-3 rounded-md cursor-pointer inline-block bg-[color:var(--light-green)] text-white py-4 px-6 hover:opacity-80 border border-solid border-white">Sign up now!</a>
