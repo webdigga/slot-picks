070d3c8b4b1c00702145b6b067aa8f15040e6bb6
webdigga <webdigga42@gmail.com>
2025-03-11 13:51:56 +0000

    New menu

--- src/_includes/static/js/menuToggle/menuToggle.js
+++ src/_includes/static/js/menuToggle/menuToggle.js
@@ -2,39 +2,24 @@
   Click event that toggles menu open and close
 */
 function menuToggle() {
-  const menuButton = document.querySelector('[data-test="menuButton"]');
-  const burger = document.querySelector('[data-test="burger"]');
-  const cross = document.querySelector('[data-test="cross"]');
+  const toggleOpen = document.querySelector('[data-test="toggleOpen"]');
+  const toggleClose = document.querySelector('[data-test="toggleClose"]');
+  const menu = document.querySelector('[data-test="menu"]');
 
-  if(menuButton) {
-    menuButton.addEventListener('click', () => {
-      const menu = document.querySelector('[data-test="menu"]');
-      const menuContainer = document.querySelector('[data-test="menuContainer"]');
-      const menuWrapper = document.querySelector('[data-test="menuWrapper"]');
-
+  if(toggleOpen) {
+    toggleOpen.addEventListener('click', () => {
       if(menu) {
-        menu.classList.toggle("hidden");
-        menu.classList.toggle("menu-styles");
+        menu.classList.remove("max-lg:hidden");
+        menu.classList.add("max-lg:block");
+      }
+    });
+  }
 
-        menuButton.classList.toggle("transition");
-        menuButton.classList.toggle("transform-180");
-
-        menuContainer.classList.toggle("fixed");
-        menuContainer.classList.toggle("z-30");
-
-        menuContainer.classList.toggle("left-[50%]");
-        menuContainer.classList.toggle("translate-x-[-50%]");
-
-        menuWrapper.classList.toggle("fixed");
-        menuWrapper.classList.toggle("z-40");
-        menuWrapper.classList.toggle("bg-white");
-        menuWrapper.classList.toggle("h-screen");
-
-        burger.classList.toggle("block");
-        burger.classList.toggle("hidden");
-
-        cross.classList.toggle("block");
-        cross.classList.toggle("hidden");
+  if(toggleClose) {
+    toggleClose.addEventListener('click', () => {
+      if(menu) {
+        menu.classList.remove("max-lg:block");
+        menu.classList.add("max-lg:hidden");
       }
     });
   }
